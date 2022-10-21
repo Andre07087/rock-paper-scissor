59c63454ff24654064692232085d0fd654bd79d9
Andre Alvarados <andre07087@gmail.com>
2022-10-21 10:51:32 -0400

    Add game function The game function plays 5 rounds and reports a winner and the results of the rounds.

--- script.js
+++ script.js
@@ -1,78 +1,88 @@
-console.log("Hello World");
-
 function getComputerChoice() {
   const rand = Math.floor(Math.random() * 3);
   let ans;
-
   switch (rand) {
     case 0:
       ans = "rock";
       break;
-
     case 1:
       ans = "paper";
       break;
-
     case 2:
       ans = "scissors";
       break;
   }
-
   return ans;
 }
 
-
-function findWinner(playerSelection, computerSelection) {
-  console.log(`The computer chose ${computerSelection}`)
-  if (playerSelection.toLowerCase() == 'rock') {
+function findRoundWinner(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() == "rock") {
     switch (computerSelection) {
-      case 'scissors':
-        console.log("Rock beats sciccors, you win")
-        break;
-      case 'paper':
-        console.log("Rock losses to paper, you lose")
-        break;
-      case 'rock':
-        console.log("Rock vs Rock, Draw")
-        break;
+      case "scissors":
+        return "You Win! Rock beats Sciccors";
+      case "paper":
+        return "You Lose! Paper beats Rock";
+      case "rock":
+        return "Draw! Rock ties Rock";
       default:
-        console.log("error")
+        console.warn("error rock vs computer");
     }
-  } else if (playerSelection.toLowerCase() == 'scissors'){
+  } else if (playerSelection.toLowerCase() == "scissors") {
     switch (computerSelection) {
-      case 'rock':
-        console.log('Scissors vs Rock, You lose')
-        break;
-      case 'paper':
-        console.log('Scissors vs Paper, You win')
-        break;
-      case 'scissors':
-        console.log('Scissors vs Scissors, draw')
-        break;
+      case "paper":
+        return "You Win! Scissors beats Paper";
+      case "rock":
+        return "You Lose! Rock beats Scissors"
+      case "scissors":
+        return "Draw! Scissors ties Scissors";
       default:
-        console.log('error')
+        console.warn("error scissors vs computer");
     }
-  } else if (playerSelection.toLowerCase() == 'paper') {
-    switch(computerSelection){
-      case 'rock':
-        console.log('Paper vs rock, you win')
-        break;
-      case 'scissors':
-        console.log('Paper vs scissors, you lose')
-        break;
-      case 'paper':
-        console.log('Paper vs Paper, draw')
-        break;
+  } else if (playerSelection.toLowerCase() == "paper") {
+    switch (computerSelection) {
+      case "rock":
+        return "You Win! Paper beats Rock";
+      case "scissors":
+        return "You Lose! Scissors beats Paper";
+      case "paper":
+        return "Draw! Paper ties Paper";
       default:
-        console.log("error")
+        console.log("error paper vs computer");
     }
   } else {
-    console.log('Invalid input, please try agian')
+    console.alert("Invalid input, please try agian");
   }
 }
 
-const playerSel = 'scissors'
+const playerSel = "scissors";
 
-findWinner(playerSel,getComputerChoice());
+// console.log(findRoundWinner(playerSel, getComputerChoice()));
 
+function game() {
+  let computerCount = 0;
+  let playerCount = 0;
+  let roundCount = 1;
 
+  for (let i = 0; i < 5; i++) {
+    let roundWinString = findRoundWinner(playerSel, getComputerChoice());
+    let resultString = roundWinString.split("!")[0];
+
+    console.log(`Round ${roundCount}: ${roundWinString}`)
+    roundCount++;
+
+    if (resultString == 'You Win'){
+      playerCount++;
+    } else if (resultString == 'You Lose') {
+      computerCount++;
+    }
+  }
+  
+  if (playerCount > computerCount) {
+    console.log(`You won the game! You win: ${playerCount} to ${computerCount}`)
+  } else if (computerCount > playerCount){
+    console.log(`You lost the game! You lost: ${computerCount} to ${playerCount}`)
+  } else {
+    console.log(`Game is a Draw! ${playerCount} to ${computerCount}`)
+  }
+}
+ game();
